875f75fb2d7da374c80eae6d3c7fee0c7c22066c
Vadim Vidnichuk <VadimXPZL@gmail.com>
2017-04-04 21:49:01 +0300

    Add M2M classes.

--- OrgWorkServer/src/main/java/com/genezis/model/M2MGroupTimeteable.java
+++ OrgWorkServer/src/main/java/com/genezis/model/M2MGroupTimeteable.java
@@ -1,7 +1,37 @@
 package com.genezis.model;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 /**
  * Created by Vadim on 04.04.2017.
  */
+@Entity
+@Table(name = "m2m_group_timeteable")
 public class M2MGroupTimeteable {
+    @ManyToOne
+        @JoinColumn(name = "IdGroup")
+    Group idOfGroupM2M;
+
+    @ManyToOne
+        @JoinColumn(name = "IdTimeteable")
+    Timeteable idTimeteableM2M;
+
+    public Group getIdOfGroupM2M() {
+        return idOfGroupM2M;
+    }
+
+    public void setIdOfGroupM2M(Group idOfGroupM2M) {
+        this.idOfGroupM2M = idOfGroupM2M;
+    }
+
+    public Timeteable getIdTimeteableM2M() {
+        return idTimeteableM2M;
+    }
+
+    public void setIdTimeteableM2M(Timeteable idTimeteableM2M) {
+        this.idTimeteableM2M = idTimeteableM2M;
+    }
 }
--- OrgWorkServer/src/main/java/com/genezis/model/M2MProfessorSubject.java
+++ OrgWorkServer/src/main/java/com/genezis/model/M2MProfessorSubject.java
@@ -1,7 +1,37 @@
 package com.genezis.model;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 /**
  * Created by Vadim on 04.04.2017.
  */
+@Entity
+@Table(name = "m2m_professor_subject")
 public class M2MProfessorSubject {
+    @ManyToOne
+    @JoinColumn(name = "IdUser")
+    User idUserM2M;
+
+    @ManyToOne
+    @JoinColumn(name = "IdSubject")
+    Subject idSubjectM2M;
+
+    public User getIdUserM2M() {
+        return idUserM2M;
+    }
+
+    public void setIdUserM2M(User idUserM2M) {
+        this.idUserM2M = idUserM2M;
+    }
+
+    public Subject getIdSubjectM2M() {
+        return idSubjectM2M;
+    }
+
+    public void setIdSubjectM2M(Subject idSubjectM2M) {
+        this.idSubjectM2M = idSubjectM2M;
+    }
 }
